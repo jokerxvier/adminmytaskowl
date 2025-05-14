6a7922756aade3eaf60488254d6acc29bc66cd4d
stanrainiersalazar@gmail.com <stanrainiersalazar@gmail.com>
2025-05-14 12:17:07 -0400

    Route Security

--- app/websocket/layout.tsx
+++ app/websocket/layout.tsx
@@ -1,8 +1,21 @@
-export default function WebSocketLayout({
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+
+export default async function WebSocketLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const cookiesData = await cookies();
+
+  // Check if cookiesData exists
+  if (cookiesData) {
+    const token = cookiesData.get("access_token")?.value;
+
+    if (!token) {
+      return redirect("/login");
+    }
+  }
   return (
     <section className="flex flex-col items-center justify-center gap-4 py-4 md:py-10 px-4 sm:px-8">
       <div className="w-full max-w-2xl text-center justify-center">

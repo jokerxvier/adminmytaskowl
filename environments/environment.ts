16e36b49fa75ee5c3338ee63ad8cd11d629f8ee3
stanrainier <stanrainiersalazar@gmail.com>
2025-08-11 16:56:48 +0800

    change env to prod

--- environments/environment.ts
+++ environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: "https://api.stage.mytaskowl.com/api/",
+  apiUrl: "https://api.mytaskowl.com/api/",
+  // apiUrl: "https://api.stage.mytaskowl.com/api/",
   // apiUrl: 'http://127.0.0.1:8000/api/',
 };
